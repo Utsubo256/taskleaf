f6832915f34c97b2ea19869321fa7245539270e5
Utsubo256 <greeeentea256@gmail.com>
2022-08-20 10:38:22 +0900

    Add login function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: session_params[:email])
+    if user&.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "ログインしました"
+    else
+      flash.now[:danger] = "Invalid email/password combination"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
